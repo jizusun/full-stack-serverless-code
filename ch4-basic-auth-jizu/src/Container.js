77f419795fa6511d62f39315aa52326b8e134160
Jizu Sun <jizu.sun@oqton.com>
2021-05-31 01:11:39 +0800

    finish ch4

---
+++ ch4-basic-auth-jizu/src/Container.js
@@ -0,0 +1,12 @@
+const styles = {
+  container: {
+    margin: '0 auto',
+    padding: '50px 100px',
+  },
+};
+
+const Container = ({ children }) => (
+  <div style={styles.container}>{children}</div>
+);
+
+export default Container;
--- ch4-basic-auth-jizu/src/index.js
+++ ch4-basic-auth-jizu/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Router from './Router';
+import 'antd/dist/antd.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
+ReactDOM.render(<Router />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
