feedff8492d9d9cc90b57d63a500dd5958d6f078
Jizu Sun <jizu.sun@oqton.com>
2021-05-30 17:02:39 +0800

    add model

---
+++ ch3-notesapp-jizu/src/graphql/queries.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getNotes = /* GraphQL */ `
+  query GetNotes($id: ID!) {
+    getNotes(id: $id) {
+      id
+      clientId
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listNotess = /* GraphQL */ `
+  query ListNotess(
+    $filter: ModelNotesFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listNotess(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        clientId
+        name
+        description
+        completed
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ ch3-notesapp-jizu/src/graphql/subscriptions.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNotes = /* GraphQL */ `
+  subscription OnCreateNotes {
+    onCreateNotes {
+      id
+      clientId
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateNotes = /* GraphQL */ `
+  subscription OnUpdateNotes {
+    onUpdateNotes {
+      id
+      clientId
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteNotes = /* GraphQL */ `
+  subscription OnDeleteNotes {
+    onDeleteNotes {
+      id
+      clientId
+      name
+      description
+      completed
+      createdAt
+      updatedAt
+    }
+  }
+`;
