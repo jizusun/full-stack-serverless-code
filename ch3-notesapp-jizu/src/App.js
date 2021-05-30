f49e35ece67f9024cea48628edb58c9922364411
Jizu Sun <jizu.sun@oqton.com>
2021-05-30 21:26:07 +0800

    update note

--- ch3-notesapp-jizu/src/App.js
+++ ch3-notesapp-jizu/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { useEffect, useReducer } from 'react';
 import { API } from 'aws-amplify';
-import { List } from 'antd';
+import { List, Input, Button } from 'antd';
 import 'antd/dist/antd.css';
 import { listNotes } from './graphql/queries';
+import { createNote as CreateNote } from './graphql/mutations';
+import { v4 as uuid } from 'uuid';
 
+const CLIENT_ID = uuid();
 const initialState = {
   notes: [],
   loading: true,
@@ -25,6 +28,15 @@ function reducer(state, action) {
       return { ...state, notes: action.notes, loading: false };
     case 'ERROR':
       return { ...state, loading: false, error: true };
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes] };
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form };
+    case 'SET_INPUT':
+      return {
+        ...state,
+        form: { ...state.form, [action.name]: action.value },
+      };
     default:
       return state;
   }
@@ -38,19 +50,57 @@ function App() {
       const notesData = await API.graphql({
         query: listNotes,
       });
-      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items });
+      dispatch({
+        type: 'SET_NOTES',
+        notes: notesData.data.listNotes.items,
+      });
     } catch (err) {
       console.log('error: ', err);
       dispatch({ type: 'ERROR' });
     }
   }
 
+  async function createNote() {
+    const { form } = state;
+    if (!form.name || !form.description) {
+      return alert('please enter a name and description');
+    }
+    const note = {
+      ...form,
+      clientId: CLIENT_ID,
+      completed: false,
+      id: uuid(),
+    };
+    dispatch({ type: 'ADD_NOTE', note });
+    dispatch({ type: 'RESET_FORM' });
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: { input: note },
+      });
+      console.log('successfully created note!');
+    } catch (err) {
+      console.log('error: ', err);
+    }
+  }
+
   function renderItem(item) {
     return (
       <List.Item style={styles.item}>
-        <List.Item.Meta title={item.name} description={item.description} />
+        <List.Item.Meta
+          title={item.name}
+          description={item.description}
+        />
       </List.Item>
     );
+  }
+
+  function onChange(e) {
+    dispatch({
+      type: 'SET_INPUT',
+      name: e.target.name,
+      value: e.target.value,
+    });
   }
 
   useEffect(() => {
@@ -59,6 +109,23 @@ function App() {
 
   return (
     <div style={styles.container}>
+      <Input
+        onChange={onChange}
+        value={state.form.name}
+        placeholder="Note Name"
+        name="name"
+        style={styles.input}
+      />
+      <Input
+        onChange={onChange}
+        value={state.form.description}
+        placeholder="Note description"
+        name="description"
+        style={styles.input}
+      />
+      <Button onClick={createNote} type="primary">
+        Create Note{' '}
+      </Button>
       <List
         loading={state.loading}
         dataSource={state.notes}
