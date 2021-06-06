b6386b2ad90657fe6a587d9d4578225e4b682e95
Jizu Sun <jizu.sun@oqton.com>
2021-06-06 19:45:01 +0800

    init amplify

---
+++ mini-vod-system/src/graphql/queries.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getVideo = /* GraphQL */ `
+  query GetVideo($id: ID!) {
+    getVideo(id: $id) {
+      id
+      name
+      url
+      description
+      likes
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listVideos = /* GraphQL */ `
+  query ListVideos(
+    $filter: ModelVideoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listVideos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        url
+        description
+        likes
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
